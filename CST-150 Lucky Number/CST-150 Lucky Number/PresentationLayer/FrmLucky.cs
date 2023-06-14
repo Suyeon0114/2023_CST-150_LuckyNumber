2936a6febb8ae8d27090d92bbcdffefcda88a9ec
Suyeon0114 <129264696+Suyeon0114@users.noreply.github.com>
2023-06-13 17:16:29 -0700

    Add files via upload

---
+++ CST-150 Lucky Number/CST-150 Lucky Number/PresentationLayer/FrmLucky.cs
@@ -0,0 +1,30 @@
+﻿/* Suyeon Park
+ * CST-150
+ * 04/30/23
+ * Activity 15
+ * Alli Adam
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST_150_Lucky_Number.PresentationLayer
+{
+    public partial class FrmLucky : Form
+    {
+        public FrmLucky()
+        {
+            InitializeComponent();
+
+            // Display the calculated Lucky Number
+            lblResult.Text = FrmInfo.luckyNum.ToString();
+        }
+    }
+}
---
+++ CST-150 Lucky Number/CST-150 Lucky Number/Program.cs
@@ -0,0 +1,19 @@
+using CST_150_Lucky_Number.PresentationLayer;
+
+namespace CST_150_Lucky_Number
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new FrmInfo());
+        }
+    }
+}
